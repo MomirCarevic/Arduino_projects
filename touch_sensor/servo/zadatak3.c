669184d751c08510b3bfd1862b6ad3771d96f121
MomirCarevic <117165454+MomirCarevic@users.noreply.github.com>
2024-11-19 23:19:35 +0100

    Update zadatak3.c

--- touch_sensor/servo/zadatak3.c
+++ touch_sensor/servo/zadatak3.c
@@ -6,8 +6,9 @@
 #define SERVO_PIN 9
 
 Servo servo;
-int pos,stanje;
-bool touch_old, touch_new;
+int pos,stanje,stanje_staro;
+bool touch_old, touch_new,flag;
+int interval;
 
 int press_button();
 
@@ -18,7 +19,10 @@ void setup()
 
     servo.attach(SERVO_PIN);
     pos = 0;
-    stanje = 3;
+  	flag = 0;
+  	stanje_staro = 2;
+  	stanje = 3;
+    interval = millis();
 
     servo.write(pos);
 }
@@ -31,60 +35,82 @@ void loop()
     case 1:
         //LEVO
         while( pos < 180 )
-        {
+        {   
+            //Serial.println("OVDE SAM 1");
             servo.write(pos);
             delay(20);
             pos++;
           
-            if(press_button())
+            if(press_button() && flag == 1)
             {
-                stanje = 2;
+                stanje_staro = stanje;
+              	stanje = 4;
+              	flag = 0;
+                interval = millis();
               	break;
             }
+          	flag = 1;
         }
-        stanje = 3;
+        interval = millis();
+        stanje = 4;
       	break;
 
     case 2:
         //DESNO
       	while( pos > 0 )
         {
+            //Serial.println("OVDE SAM 2");
             servo.write(pos);
             delay(20);
             pos--;
 
-            if(press_button())
+            if(press_button() && flag == 1 )
+            {	
+              	stanje_staro = stanje;
+                stanje = 3;
+              	flag = 0;
+                interval = millis();
+                break;
+            }
+          	flag = 1;
+        }
+        interval = millis();
+        stanje = 3;
+      	break;
+    
+     case 3:
+        //Serial.println("OVDE SAM 3");
+      	if(press_button() && millis()-interval > 100 )
+        {
+
+        	if( stanje_staro == 2 )
             {
-                stanje = 4;
+                stanje = 1;
+                flag = 0;
+                delay(100);
                 break;
             }
         }
-        stanje = 4;
-      	break;
-    
-    case 3:
-        //stop pa desno
-        delay(100);//sacekamo da se smiri debounce
-      	if(press_button())
+        break;
+
+    case 4:
+        //Serial.println("OVDE SAM 4");
+        
+        if(press_button() && millis()-interval > 100 )
+        {
+
+        	if( stanje_staro == 1 )
             {
                 stanje = 2;
+                flag = 0;
+                delay(100);
                 break;
             }
-      	break;
-      
-      case 4:
-      //stop pa levo
-        delay(100); //sacekamo da se smiri debounce
-      	if(press_button())
-            {
-                stanje = 1;
-                break;
-            }
-      	break;
+        }
+  		break;
     }
-  
-  	
 }
+
 
 int press_button()
 {
